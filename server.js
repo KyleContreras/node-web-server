0714498caa0b2f8fbfcd74e47c89c57624839f28
Kyle Contreras <kylecontreras@outlook.com>
2017-01-24 20:42:37 -0500

    Add project page.

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
+// send back json with error message
 app.get('/bad', (req, res) => {
     res.send({
        errorMessage: 'Unable to handle request' 
